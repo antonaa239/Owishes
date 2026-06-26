e2d6c28e2423536755d35c4a06ef0451e4a5c276
antonaa239 <anton2019artiukhov@yandex.ru>
2026-06-26 12:30:09 +0300

    add some allure

--- tests/api/reservations/test_create_reservation.py
+++ tests/api/reservations/test_create_reservation.py
@@ -1,3 +1,8 @@
+import allure
+
+@allure.title("Успешная бронь предмета")
 def test_create_item(reservation_client, created_item_id, reservation_data):
-    response = reservation_client.create(created_item_id, reservation_data)
-    assert response.status_code == 201
+    with allure.step("Отправить запрос POST /api/resevations/"):
+        response = reservation_client.create(created_item_id, reservation_data)
+    with allure.step("Проверка статус кода"):
+        assert response.status_code == 201
